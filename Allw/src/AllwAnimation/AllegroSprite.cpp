1447419877609fa420f23d42b401e9d25d0fdbab
Lucas Dell'Isola <ldellisola@itba.edu.ar>
2018-11-16 08:48:08 -0300

    probando cosas del gitignore

--- Allw/src/AllwAnimation/AllegroSprite.cpp
+++ Allw/src/AllwAnimation/AllegroSprite.cpp
@@ -66,57 +66,56 @@ AllegroSprite::~AllegroSprite()
 
 void AllegroSprite::draw(float x, float y)
 {
-	al_draw_scaled_rotated_bitmap(this->bitmap, this->width / 2.0, this->height / 2.0,x, y
-		,this->scaledWidth/ float(this->width),this->scaledHeight/ float(this->height), this->angle,0);
-		//, float(this->width)/ this->scaledWidth , float(this->height)/ this->scaledHeight , this->angle, 0);
+	al_draw_scaled_rotated_bitmap(this->bitmap, this->width / 2.0, this->height / 2.0,x + (this->scaledWidth / 2.0),
+		y + (this->scaledHeight / 2.0),this->scaledWidth/ float(this->width),this->scaledHeight/ float(this->height), this->angle,0);
 }
 
 void AllegroSprite::drawVerticallyMirrored(float x, float y)
 {
-	al_draw_scaled_rotated_bitmap(this->bitmap, this->width / 2.0, this->height / 2.0,
-		x, y, this->scaledWidth / this->width, this->scaledHeight / this->height, this->angle, ALLEGRO_FLIP_VERTICAL);
+	al_draw_scaled_rotated_bitmap(this->bitmap, this->width / 2.0, this->height / 2.0,x + (this->scaledWidth / 2.0),
+		y + (this->scaledHeight / 2.0), this->scaledWidth / this->width, this->scaledHeight / this->height, this->angle, ALLEGRO_FLIP_VERTICAL);
 }
 
 void AllegroSprite::drawVerticallyMirrored(float x, float y, float angle)
 {
-	al_draw_scaled_rotated_bitmap(this->bitmap, this->width / 2.0, this->height / 2.0,
-		x, y, this->scaledWidth / this->width, this->scaledHeight / this->height, degreesToRadians(angle), ALLEGRO_FLIP_VERTICAL);
+	al_draw_scaled_rotated_bitmap(this->bitmap, this->width / 2.0, this->height / 2.0,x + (this->scaledWidth / 2.0),
+	y + (this->scaledHeight / 2.0), this->scaledWidth / this->width, this->scaledHeight / this->height, degreesToRadians(angle), ALLEGRO_FLIP_VERTICAL);
 }
 
 void AllegroSprite::drawVerticallyMirrored(float x, float y, float height, float width)
 {
-	al_draw_scaled_rotated_bitmap(this->bitmap, this->width / 2.0, this->height / 2.0,
-		x, y, width / this->width, height / this->height, this->angle, ALLEGRO_FLIP_VERTICAL);
+	al_draw_scaled_rotated_bitmap(this->bitmap, this->width / 2.0, this->height / 2.0,x + (this->scaledWidth / 2.0),
+	y + (this->scaledHeight / 2.0), width / this->width, height / this->height, this->angle, ALLEGRO_FLIP_VERTICAL);
 }
 
 void AllegroSprite::drawHorizontallyMirrored(float x, float y)
 {
-	al_draw_scaled_rotated_bitmap(this->bitmap, this->width / 2.0, this->height / 2.0,
-		x, y, this->scaledWidth / this->width, this->scaledHeight / this->height, this->angle, ALLEGRO_FLIP_HORIZONTAL);
+	al_draw_scaled_rotated_bitmap(this->bitmap, this->width / 2.0, this->height / 2.0, x + (this->scaledWidth / 2.0),
+	y + (this->scaledHeight / 2.0), this->scaledWidth / this->width, this->scaledHeight / this->height, this->angle, ALLEGRO_FLIP_HORIZONTAL);
 }
 
 void AllegroSprite::drawHorizontallyMirrored(float x, float y, float angle)
 {
-	al_draw_scaled_rotated_bitmap(this->bitmap, this->width / 2.0, this->height / 2.0,
-		x, y, this->scaledWidth / this->width, this->scaledHeight / this->height, degreesToRadians(angle), ALLEGRO_FLIP_HORIZONTAL);
+	al_draw_scaled_rotated_bitmap(this->bitmap, this->width / 2.0, this->height / 2.0,x + (this->scaledWidth / 2.0),
+	y + (this->scaledHeight / 2.0), this->scaledWidth / this->width, this->scaledHeight / this->height, degreesToRadians(angle), ALLEGRO_FLIP_HORIZONTAL);
 }
 
 void AllegroSprite::drawHorizontallyMirrored(float x, float y, float height, float width)
 {
-	al_draw_scaled_rotated_bitmap(this->bitmap, this->width / 2.0, this->height / 2.0,
-		x, y, width / this->width, height / this->height, this->angle, ALLEGRO_FLIP_HORIZONTAL);
+	al_draw_scaled_rotated_bitmap(this->bitmap, this->width / 2.0, this->height / 2.0,x + (this->scaledWidth / 2.0),
+	y + (this->scaledHeight / 2.0), width / this->width, height / this->height, this->angle, ALLEGRO_FLIP_HORIZONTAL);
 }
 
 void AllegroSprite::draw(float x, float y, float angle)
 {
-	al_draw_scaled_rotated_bitmap(this->bitmap, this->width / 2.0, this->height / 2.0,
-		x, y, this->scaledWidth / this->width, this->scaledHeight / this->height, degreesToRadians(angle) , 0);
+	al_draw_scaled_rotated_bitmap(this->bitmap, this->width / 2.0, this->height / 2.0,x + (this->scaledWidth / 2.0),
+	y + (this->scaledHeight / 2.0), this->scaledWidth / this->width, this->scaledHeight / this->height, degreesToRadians(angle) , 0);
 }
 
 void AllegroSprite::draw(float x, float y, float height, float width)
 {
-	al_draw_scaled_rotated_bitmap(this->bitmap, this->width / 2.0, this->height / 2.0,
-		x, y, width / this->width, height / this->height, this->angle, 0);
+	al_draw_scaled_rotated_bitmap(this->bitmap, this->width / 2.0, this->height / 2.0,x + (this->scaledWidth / 2.0),
+	y + (this->scaledHeight / 2.0), width / this->width, height / this->height, this->angle, 0);
 }
 
 void AllegroSprite::setDimensions(float height, float width)
